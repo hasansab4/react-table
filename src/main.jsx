62460fa557b41ba08e41e786f0837dd7392ae5fc
hasansab4 <mmtakked@gmail.com>
2024-12-23 12:06:59 +0530

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
-  <Router>
+  <Router basename="/react-table">
     <App />
   </Router>,
 )
