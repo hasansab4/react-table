b1c44f9967e38a74b81c48e45f0e3a794ddee2c5
hasansab4 <mmtakked@gmail.com>
2024-12-23 11:44:48 +0530

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { HashRouter as Router } from 'react-router-dom';
+import { HashRouter as Router ,Route, Routes} from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <Router>
     <App />
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
   </Router>,
 )
